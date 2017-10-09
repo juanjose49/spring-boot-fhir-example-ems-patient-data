ed9a4c96badb913758b36bed6744a3b8d268a424
Juan J San Emeterio <juanjser@gmail.com>
2017-10-09 08:02:39 -0400

    Initial FHIR EMS Service commit.

---
+++ fhir-ems-service/src/main/java/com/fhirio/fhiremsservice/FhirEmsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fhirio.fhiremsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirEmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirEmsServiceApplication.class, args);
+	}
+}
