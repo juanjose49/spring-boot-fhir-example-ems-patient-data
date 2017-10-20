3bd88bab0252135972084cbe0bab201475f43d8b
bmontano6 <bmontan6@gatech.edu>
2017-10-19 21:12:13 -0400

    adding working angular 4 client

--- fhir-ems-UIclient/protractor.conf.js
+++ fhir-ems-UIclient/protractor.conf.js
@@ -1,28 +1,8 @@
-const { SpecReporter } = require('jasmine-spec-reporter');
-const { register } = require('ts-node')
+/**
+ * @author: @AngularClass
+ */
 
-exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:8080/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print () {}
-  },
-  beforeLaunch () {
-    register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-  },
-  onPrepare() {
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
-};
+/**
+ * look in ./config for protractor.conf.js
+ */
+exports.config = require('./config/protractor.conf.js').config;
