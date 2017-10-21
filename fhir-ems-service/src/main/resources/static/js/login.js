3c17c2a6d827d75ab12064eb5525c9d64f5acb6e
bmontano6 <bmontan6@gatech.edu>
2017-10-21 14:22:10 -0400

    updated UI

--- fhir-ems-service/src/main/resources/static/js/login.js
+++ fhir-ems-service/src/main/resources/static/js/login.js
@@ -6,23 +6,24 @@ function authenticate(){
     console.log("hey imn here!");
 
     var username=$('#username').val();
-    var password=$('#username').val();
+    var password=$('#password').val();
 
     if (username !== null && password !== null){
 
         var jsonBody={};
         jsonBody.userName=username;
         jsonBody.password=password;
+        jsonBody = JSON.stringify(jsonBody);
 
         $.ajax({
             type: "POST",
             url: '/api/authentication',
             data: jsonBody,
+            headers: { 'User': jsonBody },
             dataType: 'json',
             contentType: "application/json",
             success:function(data) {
-                alert(data);
-                console.log(data);
+                window.location.href = "dashboard.html?id="+data.token;
             }
         });
 
