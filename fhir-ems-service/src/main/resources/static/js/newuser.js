a6ed8d325a40530bb6de543784b9ce63617db07f
HShang7 <hshang7@gatech.edu>
2017-11-07 13:14:43 -0500

    add new user

---
+++ fhir-ems-service/src/main/resources/static/js/newuser.js
@@ -0,0 +1,49 @@
+function register(){
+
+    var newuser=$('#newuser').val();
+	
+    var newpass=$('#newpass').val();
+	
+	var repass=$('#repass').val();
+	
+	var firstname=$('#firstname').val();
+	
+	var lastname=$('#lastname').val();
+	var orgname=$('#organization').val();
+	var orgUuid=$('#orgUuid').val();
+	
+	if (newpass!==repass){
+		window.alert("Password mismatch, please try again");}
+	
+    else if (newuser !== null && newpass !== null && repass!== null){
+
+        var newUser={};
+		var organization = {};
+		organization.name= orgname;
+		newUser.userName=newuser;
+        newUser.password=newpass;
+		newUser.firstName=firstname;
+		newUser.lastName=lastname;
+		newUser.organizationUuid=orgUuid;	
+		newUser.organization=organization;
+		
+		newUser = JSON.stringify(newUser);
+        console.log(newUser);
+        $.ajax({
+            type: "POST",
+            url: '/api/user',
+            data: newUser,
+            dataType: 'json',
+            contentType: "application/json",
+            success:function(data) {
+                window.alert ("Registration succeed!");
+				window.location.href = "login.html";
+            }
+        });
+
+    }
+	else
+	{
+		window.alert ("Please input username and password!");
+	}
+}
